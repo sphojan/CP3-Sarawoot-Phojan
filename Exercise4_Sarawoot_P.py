7475721b6eccc341bcaf450e934fba231879d360
sphojan <64120168+sphojan@users.noreply.github.com>
2020-05-19 17:30:50 +0700

    Add files via upload

---
+++ Exercise4_Sarawoot_P.py
@@ -0,0 +1,10 @@
+F = 75.5
+G = 64.5
+I = 70
+C = 57.5
+print("--- Your Score ---")
+print("Foundation English :",F)
+print("General Business :",G)
+print("Introduction to Computer Systems :",I)
+print("Computer Programming :",C)
+
